fb27dcdc47199d6f5d7a6a41b0d1a7f9e2d4a4be
Justin Morris <PcHelpPros@Gmail.Com>
2018-04-05 19:16:15 -0400

    Database Added To LoginForm.

--- LoginForm/Form1.cs
+++ LoginForm/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Data.SqlClient;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -31,10 +32,34 @@ namespace LoginForm
 
         private void LoginButton_Click(object sender, EventArgs e)
         {
+              using (SqlConnection con = new SqlConnection(@"Data Source=THANATOS\SQLEXPRESS01;Initial Catalog=LoginDatabase;Integrated Security=True"))
+                // making connection
+                    try
+                    {
+                        SqlDataAdapter sda = new SqlDataAdapter("SELECT COUNT(*) FROM UserAlias WHERE UserName='" + textBox1.Text + "' AND Password='" + textBox2.Text + "'", con);
+                        DataTable dt = new DataTable(); //this is creating a virtual table  
+                        sda.Fill(dt);
+                        if (dt.Rows[0][0].ToString() == "1")
+                        {
+                            this.Hide(); // Hide The Login Form Once Login Is Successful.
+                            new MOTD().Show(); // Displays The Next Form Called Home. 
+                        }
+                        else
+                            MessageBox.Show("Invalid username or password");
 
-        }
+                    }
+                    finally
+                    {
+                        if (con.State == ConnectionState.Open)
+                        {
+                            con.Close();
+                        }
 
-        private void MotdButton_Click(object sender, EventArgs e)
+                    }
+                }
+            
+
+                    private void MotdButton_Click(object sender, EventArgs e)
         {
             // On Clicking MOTD Button In Login Form 
             // MOTD Form Will Open Up
@@ -48,5 +73,6 @@ namespace LoginForm
         {
 
         }
+            
     }
 }
